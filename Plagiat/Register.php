2e2d116d32b4c44a4716e2c23d2417889dda4593
ILHAMSULAIMAN\62882 <ilham280598@gmail.com>
2022-08-15 22:03:33 +0700

    update terbaru kurang winnowing

--- Plagiat/Register.php
+++ Plagiat/Register.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 include 'config.php';
 
@@ -6,26 +6,28 @@ error_reporting(0);
 
 session_start();
 
-if (isset($_SESSION['username'])) {
+if (isset($_SESSION['nim'])) {
     header("Location: index.php");
 }
 
 if (isset($_POST['submit'])) {
-	$username = $_POST['username'];
+	$nim = $_POST['nim'];
+	$nama = $_POST['nama'];
 	$email = $_POST['email'];
-	$password = md5($_POST['password']);
-	$cpassword = md5($_POST['cpassword']);
+	$password = $_POST['password'];
+	$cpassword = $_POST['cpassword'];
 
 	if ($password == $cpassword) {
-		$sql = "SELECT * FROM users WHERE email='$email'";
+		$sql = "SELECT * FROM user WHERE nim='$nim'";
 		$result = mysqli_query($conn, $sql);
 		if (!$result->num_rows > 0) {
-			$sql = "INSERT INTO users (username, email, password)
-					VALUES ('$username', '$email', '$password')";
+			$sql = "INSERT INTO user (nim, nama, email, password)
+					VALUES ('$nim', '$nama', '$email', '$password')";
 			$result = mysqli_query($conn, $sql);
 			if ($result) {
 				echo "<script>alert('Wow! User Registration Completed.')</script>";
-				$username = "";
+				$nim = "";
+				$nama = "";
 				$email = "";
 				$_POST['password'] = "";
 				$_POST['cpassword'] = "";
@@ -35,7 +37,7 @@ if (isset($_POST['submit'])) {
 		} else {
 			echo "<script>alert('Woops! Email Already Exists.')</script>";
 		}
-		
+
 	} else {
 		echo "<script>alert('Password Not Matched.')</script>";
 	}
@@ -53,23 +55,27 @@ if (isset($_POST['submit'])) {
 
 	<link rel="stylesheet" type="text/css" href="style.css">
 
-	<title>Register Form to STIMIK</title>
+	<title>Register Form </title>
 </head>
-<body>
+<body  style="background-image: 2.jpg;">
 	<div class="container">
 		<form action="" method="POST" class="login-email">
             <p class="login-text" style="font-size: 2rem; font-weight: 800;">Register</p>
 			<div class="input-group">
-				<input type="text" placeholder="Username" name="username" value="<?php echo $username; ?>" required>
+				<input type="text" placeholder="Masukan Nim" name="nim" value="<?php echo $nim; ?>" required>
 			</div>
 			<div class="input-group">
-				<input type="email" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
+				<input type="text" placeholder="Masukan Nama" name="nama" value="<?php echo $nama; ?>" required>
+			</div>
+
+			<div class="input-group">
+				<input type="email" placeholder="Masukan Email" name="email" value="<?php echo $email; ?>" required>
 			</div>
 			<div class="input-group">
-				<input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
+				<input type="password" placeholder="Masukan Password" name="password"  required>
             </div>
             <div class="input-group">
-				<input type="password" placeholder="Confirm Password" name="cpassword" value="<?php echo $_POST['cpassword']; ?>" required>
+				<input type="password" placeholder="Confirm Password" name="cpassword"  required>
 			</div>
 			<div class="input-group">
 				<button name="submit" class="btn">Register</button>
--- Plagiat/index.php
+++ Plagiat/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 include 'config.php';
 
@@ -6,22 +6,22 @@ session_start();
 
 error_reporting(0);
 
-if (isset($_SESSION['username'])) {
-    header("Location: dashboard.php");
+if (isset($_SESSION['nama'])) {
+    header("Location: user/pengguna.php");
 }
 
 if (isset($_POST['submit'])) {
-	$email = $_POST['email'];
-	$password = md5($_POST['password']);
+	$nim = $_POST['nim'];
+	$password = $_POST['password'];
 
-	$sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+	$sql = "SELECT * FROM user WHERE nim='$nim' AND password='$password'";
 	$result = mysqli_query($conn, $sql);
 	if ($result->num_rows > 0) {
 		$row = mysqli_fetch_assoc($result);
-		$_SESSION['username'] = $row['username'];
-		header("Location: dashboard.php");
+		$_SESSION['nama'] = $row['nama'];
+		header("Location: user/pengguna.php");
 	} else {
-		echo "<script>alert('Woops! Email or Password is Wrong.')</script>";
+		echo "<script>alert('Woops! Nim Atau Password anda Salah.')</script>";
 	}
 }
 
@@ -37,17 +37,17 @@ if (isset($_POST['submit'])) {
 
 	<link rel="stylesheet" type="text/css" href="style.css">
 
-	<title>Login Form to STIMIK</title>
+	<title>Login</title>
 </head>
 <body>
 	<div class="container">
 		<form action="" method="POST" class="login-email">
 			<p class="login-text" style="font-size: 2rem; font-weight: 800;">Login</p>
 			<div class="input-group">
-				<input type="email" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
+				<input type="nim" placeholder="Masukan Nim" name="nim" value="<?php echo $nim; ?>" required>
 			</div>
 			<div class="input-group">
-				<input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
+				<input type="password" placeholder="Password" name="password"  required>
 			</div>
 			<div class="input-group">
 				<button name="submit" class="btn">Login</button>
